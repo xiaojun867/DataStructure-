dee2d091118ba9ea345e06071312be5ec64f59af
wangxiaojun1a@126.com <wangxiaojun1a@126.com>
2021-05-28 17:22:00 +0800

    浙江大学

---
+++ Test/FirstTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class FirstTest
+    {
+        public void ConsoleMethod(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                // Console.Write(i);
+            }
+        }
+
+        public void ConsoleMethod2(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            ConsoleMethod2(n - 1);
+
+            // Console.Write(n);
+        }
+
+        //int[] nums = new int[] { 4, -3, 5, -2, -1, 2, -6, -2  };
+        public int MaxSubseqSum2(int[] nums)
+        {
+            int MaxSum = 0;
+            int Sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    Sum += nums[j];
+                    Console.WriteLine(Sum);
+                    if (Sum > MaxSum)
+                    {
+                        MaxSum = Sum;
+                    }
+                }
+            }
+            return MaxSum;
+        }
+
+        public int MaxSubseqSum3(int[] nums)
+        {
+            int MaxSum = 0;
+            int Sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Sum += nums[i];
+                if (Sum > MaxSum)
+                {
+                    MaxSum = Sum;
+                }
+                else if (Sum < 0)
+                {
+                    Sum = 0;
+                }
+            }
+            return MaxSum;
+        }
+
+
+        public int GetMax(int a,int b,int c)
+        {
+            return a > b ? a > c ? a : c : b > c ? b : c;
+        }
+
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FirstTest firstTest = new FirstTest();
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //firstTest.ConsoleMethod(100000);
+
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+
+
+            //stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //firstTest.ConsoleMethod2(100000);
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+
+
+            //int[] nums = new int[] { 4, -3, 5, -2, -1, 2, -6, -2 };
+            //Console.WriteLine(firstTest.MaxSubseqSum3(nums));
+            Console.WriteLine(firstTest.GetMax(45, 5, 67));
+
+        }
+    }
+}
